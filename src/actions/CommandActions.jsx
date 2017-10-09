e03966ae15465bc7810382d85055f69186c23a50
Oi Ryusuke <myr.ryusuke@gmail.com>
2017-10-09 18:12:08 +0900

    :+1: add Action signin/signup/signout
    アクションを追加 サインイン/サインアップ/サインアウト

--- src/actions/CommandActions.jsx
+++ src/actions/CommandActions.jsx
@@ -1,5 +1,25 @@
-
+export const SIGNUP = `SIGNUP`;
+export const SIGNIN = `SIGNIN`;
+export const SIGNOUT = `SIGNOUT`;
 export const GENERATE_PASSWORD = `GENERATE_PASSWORD`;
+
+export function signUp() {
+    return {
+        type: SIGNUP
+    };
+}
+
+export function signIn() {
+    return {
+        type: SIGNIN
+    };
+}
+
+export function signOut() {
+    return {
+        type: SIGNOUT
+    };
+}
 
 export function generatePassword(seed_password, seed_id) {
     return {
