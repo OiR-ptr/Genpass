659b88613d439826da47601ac0d43fb4aa110d68
Oi Ryusuke <myr.ryusuke@gmail.com>
2017-11-05 00:19:51 +0900

    :arrow_right: change reducer switch-case format
    リデューサーの分岐条件を定数化

--- src/reducers/genpass.jsx
+++ src/reducers/genpass.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import {GENERATE_PASSWORD} from "../actions/CommandActions";
 
 const initialState = {
 };
 
 export default function genpass(state = initialState, action) {
     switch(action.type) {
-        case `GENERATE_PASSWORD`:
+        case GENERATE_PASSWORD:
             return state;
 
         default:
