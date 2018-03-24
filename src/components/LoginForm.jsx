e63127ab96bc8b420d991537dc4c3b70cf1530a2
Oi Ryusuke <myr.ryusuke@gmail.com>
2018-03-25 00:43:04 +0900

    :+1: separate firebase config from loginform
    ログインフォームからFirebase用の設定を切り分ける

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,23 +1,13 @@
 import React from "react";
+import fs from "fs";
 import * as firebase from "firebase";
 import Paper from "material-ui/Paper";
 import FlatButton from "material-ui/FlatButton";
 
 export default class LoginForm extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
     componentWillMount() {
-        var config = {
-            apiKey: "AIzaSyBc_L-c7Xl-ovrZe_vz3ISccmUbEAkwAzg",
-            authDomain: "password-generator-aed1d.firebaseapp.com",
-            databaseURL: "https://password-generator-aed1d.firebaseio.com",
-            projectId: "password-generator-aed1d",
-            storageBucket: "password-generator-aed1d.appspot.com",
-            messagingSenderId: "715834437227"
-          };
-          firebase.initializeApp(config);
+        var config = JSON.parse(fs.readFileSync('./firebaseauth.json', 'utf-8'));
+        firebase.initializeApp(config);
     }
 
     render() {
